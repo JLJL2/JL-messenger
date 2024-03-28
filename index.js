fb4cbfa7dcb73d0be077c25d852dbb7b465298b8
Vainqueurgab <165157345+Vainqueurgab@users.noreply.github.com>
2024-03-28 14:35:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -534,7 +534,7 @@ async function accountLogin(state, enableCommands = [], prefix, admin = []) {
 							const { threadID } = event;
 
 					if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == userid)) {
-					api.changeNickname(`》 ${prefix} 《 ❃ ➠█▚▞▌▐◖▐▅▚▅▀▅█▚▌ `, threadID, userid);
+					api.changeNickname(`<< ${prefix} >> 𝑪𝑯𝑨𝑻-𝑮𝑷𝑻 `, threadID, userid);
 
 let gifUrls = [
 		'https://i.imgur.com/209z0iM.mp4',
